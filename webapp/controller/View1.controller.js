b57df961b42546b439f939b6a7e6d538d1139a14
N Rajesh <rajesh.n@artihcus.com>
2024-04-19 10:54:06 +0000

    defualt,named,global models

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,37 +1,43 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
+
     "sap/ui/model/json/JSONModel"
 
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageToast) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("fiori.controller.View1", {
             onInit: function () {
-
-            },
-            onClick: function (oEvent) {
-                debugger
-                const num1 = this.getView().byId("input1").getValue()
-                const num2 = this.getView().byId("input2").getValue()
-                let add = (+num1) + (+num2)
-                alert(add)
-
-
-            },
-            oJson: function () {
-
-                debugger;
-
-                name: "ramesh";
-                age: 56
-
+                const details = {
+                    Emp:{
+                    name:"ramesh",
+                    age: 56
+                    }
+                }
+                const PHONE = {
+                    MOBILE:{
+                    Phoneno:938137446,
+                
+                    }
+                }
+               const json  = new JSONModel(details);
+               const json1  = new JSONModel(PHONE);
+               this.getView().setModel(json);
+               this.getView().setModel(json1,"JSON1");
 
             }
+            
+           
+
+       
+            
+            
+
+
         
         });
     });
